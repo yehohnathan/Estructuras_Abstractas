e016a5567542805829cd3b6dbdfd3376e04de582
Yehohnathan Miranda Vigil <yehohnathan2002@gmail.com>
2024-01-13 21:25:21 -0600

    Uso de clase con dos parámetros en el Template
    Signed-off-by: Yehohnathan Miranda Vigil <yehohnathan2002@gmail.com>

--- TrabajosPrevios/Sesion8/3.dosParametrosTemplate.cpp
+++ TrabajosPrevios/Sesion8/3.dosParametrosTemplate.cpp
@@ -1,6 +1,13 @@
+/*
+    Este es un template genérico con una clase que se encarga de multiplicar dos
+    números, que pueden ser del tipo diferente de dato gracias a que usa dos 
+    parámetros T y U.
+*/
+
 #include <iostream>
 using namespace std;
 
+// Creo un template con dos parámetros para realizar una multiplicación
 template <typename T, typename U>
 T multiply(T a, U b){
     return a * b;
