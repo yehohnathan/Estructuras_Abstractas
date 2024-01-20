a9de1cad7c092997dcf65fbc02b2e220b07abd58
Yehohnathan Miranda Vigil <yehohnathan2002@gmail.com>
2024-01-20 13:16:16 -0600

    Uso de las entradas con input() en Python
    Signed-off-by: Yehohnathan Miranda Vigil <yehohnathan2002@gmail.com>

---
+++ TrabajosPrevios/Sesion10/7.entradas.py
@@ -0,0 +1,10 @@
+"""
+    Para solicitarle al usuario es necesario utilizar la siguiente función:
+        input(prompt)
+"""
+
+# Usando input para que el usuario ingrese un número
+num = int(input("Ingrese un número: "))
+
+print("Tu ingresaste: ", num)
+print("Tipo de dato de num: ", type(num))
