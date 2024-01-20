9dc516a513e4762617dd086e49b98017cb2181e7
Yehohnathan Miranda Vigil <yehohnathan2002@gmail.com>
2024-01-20 13:13:50 -0600

    Uso de comentarios
    Signed-off-by: Yehohnathan Miranda Vigil <yehohnathan2002@gmail.com>

---
+++ TrabajosPrevios/Sesion10/2.comentarios.py
@@ -0,0 +1,19 @@
+"""Comentarios simples"""
+# Se declara y se inicializan dos variables:
+num1 = 6
+num2 = 2
+
+# print the output
+print = 'Eric Cartman'  # nombre de SouthPark en un string
+
+# Esto es un comentario extenso
+# y este se extiende
+# a variables lineas
+
+""" Esto también es
+    un perfector ejemplo
+    de comentarios en varias lineas """
+
+''' Esto también es
+    un perfector ejemplo
+    de comentarios en varias lineas '''
