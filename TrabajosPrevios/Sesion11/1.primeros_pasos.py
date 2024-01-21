3add2b7d6cfa7ede4c8178c8a70b088e9d053d17
Yehohnathan Miranda Vigil <yehohnathan2002@gmail.com>
2024-01-20 22:31:57 -0600

    Comprobación de descarga de python
    Signed-off-by: Yehohnathan Miranda Vigil <yehohnathan2002@gmail.com>

---
+++ TrabajosPrevios/Sesion11/1.primeros_pasos.py
@@ -0,0 +1,9 @@
+# Print para comprobar que python esta instalado en la compu
+print("Hola Mundo desde python")
+
+# Uso de variables en python
+contador = 0    #Inicializo la variable en cero
+
+contador = 34   # Le cambio el valor a la variable contador
+
+print("El valor de contador es:", contador)  # Output 34
