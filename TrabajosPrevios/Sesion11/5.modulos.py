2718ea52bc8e53601940a514d4d63452578ad682
Yehohnathan Miranda Vigil <yehohnathan2002@gmail.com>
2024-01-20 23:15:59 -0600

    Uso de modulo math
    Signed-off-by: Yehohnathan Miranda Vigil <yehohnathan2002@gmail.com>

---
+++ TrabajosPrevios/Sesion11/5.modulos.py
@@ -0,0 +1,7 @@
+# import standard math module
+import math
+from math import pi     # Para o tener que escribir math.pi
+# from math import *    -> importa todo
+# import math as m      .> para ecribir m.pi
+
+print(pi)   # Output 3.1415...
