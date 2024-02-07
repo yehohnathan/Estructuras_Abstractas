e02b5dad4b30c08a65c87ef0298ace86a2f75292
Yehohnathan Miranda Vigil <yehohnathan2002@gmail.com>
2024-02-07 00:11:19 -0600

    Archivo para probrar la clase DataFrame
    Signed-off-by: Yehohnathan Miranda Vigil <yehohnathan2002@gmail.com>

--- Tareas/TAREA_CINCO/src/Manejo_CSV.py
+++ Tareas/TAREA_CINCO/src/Manejo_CSV.py
@@ -1,6 +1,7 @@
 # ----------- # Se cargan las librerías necesarias el programa # ------------ #
 # import pandas as pd                 # Para manejo de CSV
 # import matplotlib.pyplot as pt      # Para crear gráficar con la info del CSV
+import sys                          # Para terminar el programa
 
 # ----------- # Se importa la clase para manipular DataFrames # ------------- #
 from DataFrame import DataFrame
@@ -10,7 +11,7 @@ from DataFrame import DataFrame
 mi_dataFrame = DataFrame()
 
 # Ingresar la dirección donde se encuentra el csv
-ruta_csv = "../TAREA_CINCO/Material_Practica/T_T100I_MARKET_ALL_CARRIE.csv"
+ruta_csv = "../TAREA_CINCO/Material_Practica/T_T100I_MARKET_ALL_CARRIER.csv"
 
 # instancia el csv en un DataFrame, siempre comprobando que si exista el csv
 mi_dataFrame.setCSV(ruta_csv)
@@ -19,6 +20,7 @@ mi_dataFrame.setCSV(ruta_csv)
 # programa
 if mi_dataFrame.getCSV().empty:
     print("Se finalizó el programa.")
+    sys.exit()
 
 # ------------- # Limpieza y preparación de datos del DatFrame# ------------- #
 # Creación de la lista que contiene las columnas a eliminar del csv
@@ -38,4 +40,4 @@ mi_dataFrame.eliminarColumnas(columnas_a_eliminar)
 mi_dataFrame.eliminarValoresNulos()
 
 # Se imprime el contenido del dataframe
-print(mi_dataFrame.getCSV())
+mi_dataFrame.mostrarDataFrame()
