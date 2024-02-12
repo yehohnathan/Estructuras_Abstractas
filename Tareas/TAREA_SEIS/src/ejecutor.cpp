e4cd8ea0af0a2a1d604cda9e3ff04b6b89f0f373
Yehohnathan Miranda Vigil <yehohnathan2002@gmail.com>
2024-02-12 16:39:37 -0600

    Se analiza y limpia el csv
    Signed-off-by: Yehohnathan Miranda Vigil <yehohnathan2002@gmail.com>

--- Tareas/TAREA_SEIS/src/ejecutor.cpp
+++
@@ -1,7 +0,0 @@
-// Programa para compilar archivos de Python mediante el compilador de
-// windowns MSYS
-#include <cstdlib>
-
-int main() {
-    return 0;
-}
--- Tareas/TAREA_SEIS/src/main.py
+++ Tareas/TAREA_SEIS/src/main.py
@@ -24,13 +24,50 @@ try:
     # Se instancia el nombre del csv:
     name_csv = ("../TAREA_SEIS/CSV_KAGGLE/" +
                 "CAR%20DETAILS%20FROM%20CAR%20DEKHO.csv")
-except Exception as e:
+except Exception:
     print("Ha ocurrido un error. Posiblemente el kaggle.json no se encuentre",
           "en el directorio esperado **LEE EL README**.")
-    print(e)
     print("Se procederá a utilizar el CSV de respaldo.")
     name_csv = "../TAREA_SEIS/Respaldo/Respaldo_CSV.csv"
 
-# --------------------------- # Lectura del CSV # --------------------------- #
-df = pd.read_csv(name_csv)
-print(df.head())
+# --------------------- # Análisis y limpieza del CSV # --------------------- #
+# Se carga el CSV en un DataFrame:
+vehiculos_df = pd.read_csv(name_csv)
+
+# Se muestra la información del DataFrame al usuario:
+print("\n========== Datos originales del DataFrame: ==========")
+print(vehiculos_df)
+
+# Análisis exploratorio de datos:
+print("\n========== Información del DataFrame: ==========")
+vehiculos_df.info()
+
+# Verifica que no haya valores faltantes:
+if vehiculos_df.isnull().sum().any():
+    # Si hay valores faltantes, por lo que se procede a remplazarlos por
+    # valor promedio de la columna pra no desviar tanto los datos de la
+    # posible regresion o cluster.
+    print("\n========== Hay valores faltantes dentro del DataFrame.",
+          "Remplazando... ==========")
+    vehiculos_df.fillna(vehiculos_df.mean(), inplace=True)
+else:
+    print("\n========== No hay valores faltantes dentro del DataFrame",
+          "==========")
+
+# Verifica que no haya filas con datos repetidos:
+if vehiculos_df.duplicated().any():
+    print("\n========== Hay valores duplicados en el DataFrame.",
+          "Eliminando... ==========")
+    # Como si hay valores repetidos, se eliminan y se modifica el DataFrame
+    # original
+    vehiculos_df.drop_duplicates(inplace=True)
+
+    # Se restablece el indice, elimnandolo y emplazandolo del DataFrame
+    # original
+    vehiculos_df.reset_index(drop=True, inplace=True)
+else:
+    print("\n========== No hay valores duplicados en el DataFrame. ==========")
+
+# Se muestra la información del DataFrame al usuario:
+print("\n========== Datos del DataFrame posterior a la limpieza: ==========")
+print(vehiculos_df)
