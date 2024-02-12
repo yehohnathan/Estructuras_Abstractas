cce4a17e51b5b81fe47633924e7ad1030b98421f
Yehohnathan Miranda Vigil <yehohnathan2002@gmail.com>
2024-02-12 00:28:16 -0600

    Bloque de excepción para generar CSV
    En el caso de que el usuario no haya colocado el kaggle.json donde corresponde se procederá a utilizar un CSV de respaldo con la misma información que el CSV esperado.
    
    Signed-off-by: Yehohnathan Miranda Vigil <yehohnathan2002@gmail.com>

--- Tareas/TAREA_SEIS/src/main.py
+++ Tareas/TAREA_SEIS/src/main.py
@@ -1,16 +1,34 @@
 # ------------- # Importar módulos utilizados en el programa # -------------- #
-# Del modulo kaggle.api.kaggle_api_extended, se importa la clase KaggleApi para
-# interartuar con plataforma de Kaggle
-from kaggle.api.kaggle_api_extended import KaggleApi
+import pandas as pd
 
 # -------------- # Importar CSV desde la plataforma de Kaggle # ------------- #
-# Se crea un objeto desde la clase KaggleApi
-api = KaggleApi()
-# Se utiliza un método para autentificar el username y la key, de la cuenta de
-# usuario registrado en la plataforma. Accediendo al kaggle.json
-api.authenticate()
+# Bloque de excepción si no esta el kaggle.json
+try:
+    # Del modulo kaggle.api.kaggle_api_extended, se importa la clase KaggleApi
+    # para interartuar con plataforma de Kaggle
+    from kaggle.api.kaggle_api_extended import KaggleApi
 
-# Descargar el archivo CSV "CAR DETAILS FROM CAR DEKHO.csv", del conjunto de
-# datos de car-details-dataset del usuario akshaydattatraykhare3.
-api.dataset_download_file('akshaydattatraykhare/car-details-dataset',
-                          file_name='CAR DETAILS FROM CAR DEKHO.csv')
+    # Se crea un objeto desde la clase KaggleApi
+    api = KaggleApi()
+
+    # Se utiliza un método para autentificar el username y la key, de la cuenta
+    # de usuario registrado en la plataforma. Accediendo al kaggle.json
+    api.authenticate()
+
+    # Descargar el archivo CSV "CAR DETAILS FROM CAR DEKHO.csv", del conjunto
+    # de datos de car-details-dataset del usuario akshaydattatraykhare3.
+    api.dataset_download_file('akshaydattatraykhare/car-details-dataset',
+                              file_name='CAR DETAILS FROM CAR DEKHO.csv')
+
+    # Se instancia el nombre del csv:
+    name_csv = "../TAREA_SEIS/CAR%20DETAILS%20FROM%20CAR%20DEKHO.csv"
+except Exception as e:
+    print("Ha ocurrido un error. Posiblemente el kaggle.json no se encuentre",
+          "en el directorio esperado **LEE EL README**.")
+    print(e)
+    print("Se procederá a utilizar el CSV de respaldo.")
+    name_csv = "../TAREA_SEIS/Respaldo/Respaldo_CSV.csv"
+
+# --------------------------- # Lectura del CSV # --------------------------- #
+df = pd.read_csv(name_csv)
+print(df.head())
